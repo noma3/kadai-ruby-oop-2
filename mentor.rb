ce8c2117c04f7486ddb49bccdd7b56e8213bc179
noma <noma.developper@gmail.com>
2021-10-27 05:57:36 +0000

    first commit

---
+++ mentor.rb
@@ -0,0 +1,34 @@
+# クラス定義
+class Mentor
+
+  # インスタンスが持つ変数（値）
+  attr_accessor :name
+
+  # インスタンスを初期化するための、特別なメソッド
+  def initialize(name)
+    self.name = name
+  end
+
+  # インスタンスが持つメソッド（処理）
+  def job
+    puts "#{name}です。私は現役のITプロフェッショナルです。"
+  end
+end
+
+# クラス定義
+class RailsMentor < Mentor
+
+  # インスタンスが持つメソッド（処理）
+  def job
+    puts "#{name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+  end
+end
+
+
+# インスタンスの生成と、変数への代入
+kimeragi= Mentor.new('煌木')
+akaide= RailsMentor.new('赤出')
+
+# インスタンスの使用
+kimeragi.job
+akaide.job
